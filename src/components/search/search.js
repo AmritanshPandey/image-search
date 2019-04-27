3932016291c28b52e2f6f9a317f70e18f4bac036
Apoorva221 <apoorvasharma.888@gmail.com>
2019-04-28 00:28:33 +0530

    new changes

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import TextField from 'material-ui/TextField';
-import Select from '@material-ui/core/Select';
+import SelectField from 'material-ui/SelectField';
 import MenuItem from '@material-ui/core/MenuItem';
 import axios from 'axios';
+
 
 
 class Search extends Component {
@@ -15,11 +16,16 @@ class Search extends Component {
     };
 
     onTextChange = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}$safesearch=true`)
+                .then(res => this.setState({ images: res.data.hits }))
+                .catch(err => console.log(err));
+        });
     };
-
+onAmountChange = (e,index,value) => this.setState({amount: value});
 
     render() {
+        console.log(this.state);
         return (
             <div>
                 <TextField
@@ -29,18 +35,19 @@ class Search extends Component {
                     floatingLabelText="Search for Images"
                     fullWidth={true}
                 />
-                <br/>
-                <Select
+                <br />
+                <SelectField
                     name="amount"
                     floatingLabelText="Amount"
                     value={this.state.amount}
                     onChange={this.onAmountChange}
-                    >
+                >
+                    <MenuItem value={5}>5</MenuItem>
                     <MenuItem value={10}>10</MenuItem>
                     <MenuItem value={10}>15</MenuItem>
                     <MenuItem value={30}>30</MenuItem>
                     <MenuItem value={50}>50</MenuItem>
-                </Select>
+                </SelectField>
             </div>
         )
     }
